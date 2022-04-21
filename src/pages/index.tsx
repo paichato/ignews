81806eb131d6a24e4b9b66b594cd364353231ce4
paichato <tsenane14@gmail.com>
2022-04-21 21:53:43 +0200

    added SigniButton styles +++

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,24 @@ import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Inicio |ig.news</title>
       </Head>
 
-      <h1 className={styles.title}>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />{" "}
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </div>
   );
 }
