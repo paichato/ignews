9805e9c42ed484f2205661030e5cfbc0b13e31b2
paichato <tsenane14@gmail.com>
2022-04-21 21:32:11 +0200

    added SigniButton styles +

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,10 +1,16 @@
 import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
 
 export const SignInButton = () => {
-  return (
+  return isUserLoggedIn ? (
     <button type="button" className={styles.signInButton}>
-      <FaGithub /> Sign in with Github
+      <FaGithub color="#04d361" /> Marlon D Jesus{" "}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417" /> Sign in with Github
     </button>
   );
 };
