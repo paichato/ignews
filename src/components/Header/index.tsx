541cbcfec81428f80085b0a70d60d18fde6519f5
paichato <tsenane14@gmail.com>
2022-04-21 20:55:14 +0200

    added header structure + styles ++

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a>Home</a>
+          <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
       </div>
