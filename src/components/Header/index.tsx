5dcf29c14b8dd8a6f3da3e357f00830b6b1da33b
paichato <tsenane14@gmail.com>
2022-04-21 20:31:19 +0200

    added header structure + styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
+import styles from "./styles.module.scss";
+
 export const Header = () => {
   return (
-    <header>
+    <header className={styles.headerContainer}>
       <div>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
