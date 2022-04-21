8d87ec1f18beb504c95a16d30ccd2e34a18c785e
paichato <tsenane14@gmail.com>
2022-04-22 01:16:29 +0200

    added signout method

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,19 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, useSession, signOut } from "next-auth/react";
 
 export const SignInButton = () => {
   const { data: session } = useSession();
   // const isUserLoggedIn = useSession();
 
   return session ? (
-    <button type="button" className={styles.signInButton}>
-      <FaGithub color="#04d361" /> Marlon D Jesus{" "}
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <FaGithub color="#04d361" /> {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
