44c9ac6b7384dce11938e4b83b8d0bd2e5ede54d
paichato <tsenane14@gmail.com>
2022-04-21 23:22:17 +0200

    addedstripe config +

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from "./styles.module.scss";
 
-export const SubscribeButton = () => {
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home({ product }: HomeProps) {
             Get access to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
