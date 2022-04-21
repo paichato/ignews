c780ffbed946613606ae58e79ffc67b236f11908
paichato <tsenane14@gmail.com>
2022-04-21 20:25:14 +0200

    added header structure

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+  return (
+    <header>
+      <div>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
