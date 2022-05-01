1fe3cb39c0828eca5e6efd84ae58d9f0cab996a2
paichato <tsenane14@gmail.com>
2022-05-02 01:19:34 +0200

    added method update user faunadb

--- src/pages/api/auth/subscribe.ts
+++ src/pages/api/auth/subscribe.ts
@@ -1,14 +1,31 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
+import { fauna } from "../../../services/fauna";
 import { stripe } from "../../../services/stripe";
+import { query as q } from "faunadb";
+
+type User = {
+  ref: {
+    id: string;
+  };
+};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const session = await getSession({ req });
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
 
     const stripeCustomer = await stripe.customers.create({
       email: session.user.email,
     });
+
+    await fauna.query(
+      q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+        data: { stripe_customer_id: stripeCustomer.id },
+      })
+    );
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
       customer: stripeCustomer.id,
