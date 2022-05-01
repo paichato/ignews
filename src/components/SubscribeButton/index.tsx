bea2c3943dbea8c5aab3279040f645813c240f9c
paichato <tsenane14@gmail.com>
2022-05-01 13:43:18 +0200

    added checkout logic

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJS } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -8,15 +10,32 @@ interface SubscribeButtonProps {
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
   const { data: session, status } = useSession();
 
-  const handleSubscribe = () => {
+  const handleSubscribe = async () => {
     if (status === "unauthenticated") {
       signIn("github");
       return;
     }
+
+    try {
+      const response = await api.post("auth/subscribe");
+
+      const { sessionId } = response.data;
+      const stripe = await getStripeJS();
+
+      if (stripe) console.log("STRIPE:", stripe);
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+    }
   };
 
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      onClick={handleSubscribe}
+      type="button"
+      className={styles.subscribeButton}
+    >
       Subscribe now
     </button>
   );
--- src/services/stripe-js.ts
+++ src/services/stripe-js.ts
@@ -2,4 +2,5 @@ import { loadStripe } from "@stripe/stripe-js";
 
 export const getStripeJS = async () => {
   const stripeJs = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+  return stripeJs;
 };
