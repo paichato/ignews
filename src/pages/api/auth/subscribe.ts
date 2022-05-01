b043bc49a34cf7dc71c9da819a3d3200da421c49
paichato <tsenane14@gmail.com>
2022-05-01 12:59:54 +0200

    added subscribe page +

--- src/pages/api/auth/subscribe.ts
+++ src/pages/api/auth/subscribe.ts
@@ -1,7 +1,32 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { stripe } from "../../../services/stripe";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
+    const session = await getSession({ req });
+
+    const stripeCustomer = await stripe.customers.create({
+      email: session.user.email,
+    });
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: stripeCustomer.id,
+      payment_method_types: ["card"],
+      billing_address_collection: "required",
+      line_items: [
+        {
+          price: "price_1Kr6PCHN4kjuy5s0YJi7RkU7",
+          quantity: 1,
+        },
+      ],
+      mode: "subscription",
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    });
+
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id });
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method not allowed");
